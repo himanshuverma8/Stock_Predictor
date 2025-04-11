3d32399c49be1b4757488a376855a0ab35c21c98
Himanshu Verma <hvinprimary@gmail.com>
2025-04-12 03:29:57 +0530

    add margin to lighting logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
     <div className="flex gap-2">
       {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
       <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mr-4">
           <div className="w-10 rounded-full">
             <img
               alt="hv's logo"
