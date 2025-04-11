b85ef1391b9edc4095c5381df4efbe81e9fb71cd
Himanshu Verma <hvinprimary@gmail.com>
2025-04-12 03:34:51 +0530

    add footer component to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
 import StockChart from './components/StockChart.jsx'
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="main-container min-h-screen" data-theme="black">
       <Navbar />
       <StockChart />
+      <Footer />
       </div>
     </>
   )
